6524383f33f3ca90f399f099645bdc14e197f7d3
Zachary Gaines <zacharygaines28@gmail.com>
2021-04-13 17:34:59 -0700

    map.js tests done

--- map.js
+++ map.js
@@ -19,7 +19,8 @@ Output:
 */
 
 export function makeArrayOfNamesWithMap(arr) {
-    return [];
+    const newArray = arr.map(item => item.name)
+    return newArray;
 }
 
 /*
@@ -33,7 +34,12 @@ Output:
 ]*/
 
 export function makeArrayWithIsHungry(arr) {
-    return [];
+    const newArray = arr.map(item => item)
+    newArray.map(item => {
+        item.isHungry = true
+    })
+
+    return newArray;
 }
 
 /*
@@ -47,12 +53,19 @@ Output:
 ]*/
 
 export function makeShoutingArray(arr) {
-    return [];
+    const newArray = arr.map(item => ({
+        name: item.name.toUpperCase(), type: item.type
+    }))
+
+    return newArray;
 }
 
 
-/*
 
+
+
+/*
+ 
 Output:
 ['spotdog', 'roverdog', 'jumpyfrog', einsteincat']
 */
@@ -63,7 +76,7 @@ export function makeStringArray(arr) {
 
 /*
 Output:
-
+ 
 [
     [
         ['name', 'spot'], 
